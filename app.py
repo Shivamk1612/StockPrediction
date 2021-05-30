ba27ba3c7f2e8b66fbb7a85e11ece947d628d963
Shivamk1612 <53030306+Shivamk1612@users.noreply.github.com>
2021-05-31 02:00:45 +0530

    a

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from fbprophet import Prophet
+
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
@@ -42,24 +42,3 @@ def plot_raw_data():
 	st.plotly_chart(fig)
 	
 plot_raw_data()
-
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
-
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
-
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
-    
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
-
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
